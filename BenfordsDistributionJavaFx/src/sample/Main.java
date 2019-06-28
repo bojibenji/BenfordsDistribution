a8ae65aa1b241d7acb7fa03845f403f5eb21b648
bojibenji <bbenji@ggc.edu>
2019-06-28 12:38:21 -0400

    Testing

--- BenfordsDistributionJavaFx/src/sample/Main.java
+++ BenfordsDistributionJavaFx/src/sample/Main.java
@@ -11,10 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello");
+        primaryStage.setTitle("Hello world");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        //hello world
+
     }
 
 
